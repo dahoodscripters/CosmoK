23d72e0753e0f02db53d64e6160c24eb6598beac
RBLXSCRIPTERSCODE <110972398+dahoodscripters@users.noreply.github.com>
2022-08-12 16:48:21 -0700

    Update RankGetter.lua

--- API/RankGetter.lua
+++ API/RankGetter.lua
@@ -1,6 +1,6 @@
-local RankID = 79833425
+local RankID = 1
 local RankNumeric = 0
-local GroupID = 14007434
+local GroupID = 15635946
 local RankName = "Premium"
 local data = game:service"HttpService":JSONDecode(game:HttpGet("https://groups.roblox.com/v1/groups/"..tostring(GroupID).."/roles"))
 for i,v in pairs(data.roles) do
